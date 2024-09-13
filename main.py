dac7751b0b422d365d144b7734cc53b0b2b75f42
diegodizmar <diego.dizmar@gmail.com>
2024-09-13 14:25:47 +0200

    Update and rename main.py.py to main.py
    Actualizados textos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-﻿import streamlit as st
+import streamlit as st
 from math import sqrt
 
 def calcular_op_z(orelleta_z, orelleta_x, orelleta_y, CdG_X, CdG_Y, Lonxitude_min_Eslinga):
@@ -8,7 +8,7 @@ def calcular_op_z(orelleta_z, orelleta_x, orelleta_y, CdG_X, CdG_Y, Lonxitude_mi
     discriminant = b**2 - 4 * a * c
 
     if discriminant < 0:
-        st.error("Discriminante negativo, no hay solución real.")
+        st.error("Discriminante negativo, non hay solución real.")
         return None
     
     z1 = (-b + sqrt(discriminant)) / (2 * a)
@@ -17,9 +17,10 @@ def calcular_op_z(orelleta_z, orelleta_x, orelleta_y, CdG_X, CdG_Y, Lonxitude_mi
     return max(z1, z2)
 
 def main():
-    st.title("Cálculos de Orelletas")
+    #st.title("Cálculos de Orelletas")
+    st.title("Cálculos Diego")
 
-    st.header("Ingrese las coordenadas de las Orelletas y el centro de gravedad")
+    st.header("Cálculo de lonxitudes de eslingas, cubrir os seguintes datos")
 
     # Inicialización de session_state para evitar errores
     orelletas = ['1', '2', '3', '4']
@@ -39,7 +40,7 @@ def main():
 
     # Entradas organizadas en formato tabla con expander
     for i in range(1, 5):
-        with st.expander(f"Orelleta {i}"):
+        with st.expander(f"Coordenadas Orelleta {i}"):
             col1, col2, col3 = st.columns(3)
             with col1:
                 st.write("X")
@@ -51,7 +52,7 @@ def main():
                 st.write("Z")
                 globals()[f'Orelleta_{i}_Z'] = col3.number_input("", key=f'Orelleta {i} Z', value=st.session_state[f'Orelleta {i} Z'], format="%.2f", label_visibility="collapsed")
 
-    with st.expander("Centro de Gravedad"):
+    with st.expander("Coordenadas Centro de Gravidade"):
         col1, col2, col3 = st.columns(3)
         with col1:
             st.write("X")
@@ -63,9 +64,9 @@ def main():
             st.write("Z")
             CdG_Z = col3.number_input("", key='CdG_Z', value=st.session_state['CdG_Z'], format="%.2f", label_visibility="collapsed")
 
-    # Separación y campo de longitud mínima de eslinga
+    # Separación e campo de lonxitude mínima de eslinga
     st.write(" ")
-    Lonxitude_min_Eslinga = st.number_input("Longitud mínima de Eslinga", value=0.0, format="%.2f")
+    Lonxitude_min_Eslinga = st.number_input("Longixude mínima de eslinga", value=0.0, format="%.2f")
 
     if st.button("Calcular"):
         try:
@@ -113,10 +114,10 @@ def main():
                 sqrt((OP_4_X - st.session_state['Orelleta 4 X'])**2 + (OP_4_Y - st.session_state['Orelleta 4 Y'])**2 + (OP_4_Z - st.session_state['Orelleta 4 Z'])**2)
             ]
 
-            # Calcular el mínimo de los primeros elementos de cada lista
+            # Calcular o mínimo dos primeros elementos de cada lista
             min_01 = min(OL_1[0], OL_2[0], OL_3[0], OL_4[0])
 
-            # Determinar coord_o basado en el valor mínimo
+            # Determinar coord_o basado no valor mínimo
             if min_01 == OL_1[0]:
                 coord_o_X, coord_o_Y, coord_o_Z = OP_1_X, OP_1_Y, OP_1_Z
             elif min_01 == OL_2[0]:
@@ -126,23 +127,26 @@ def main():
             else:
                 coord_o_X, coord_o_Y, coord_o_Z = OP_4_X, OP_4_Y, OP_4_Z
 
-            # Calcular longitudes
+            # Calcular lonxitudes
             Lonx_eslinga_orelleta_1 = sqrt((coord_o_X - st.session_state['Orelleta 1 X'])**2 + (coord_o_Y - st.session_state['Orelleta 1 Y'])**2 + (coord_o_Z - st.session_state['Orelleta 1 Z'])**2)
             Lonx_eslinga_orelleta_2 = sqrt((coord_o_X - st.session_state['Orelleta 2 X'])**2 + (coord_o_Y - st.session_state['Orelleta 2 Y'])**2 + (coord_o_Z - st.session_state['Orelleta 2 Z'])**2)
             Lonx_eslinga_orelleta_3 = sqrt((coord_o_X - st.session_state['Orelleta 3 X'])**2 + (coord_o_Y - st.session_state['Orelleta 3 Y'])**2 + (coord_o_Z - st.session_state['Orelleta 3 Z'])**2)
             Lonx_eslinga_orelleta_4 = sqrt((coord_o_X - st.session_state['Orelleta 4 X'])**2 + (coord_o_Y - st.session_state['Orelleta 4 Y'])**2 + (coord_o_Z - st.session_state['Orelleta 4 Z'])**2)
 
-            # Mostrar resultados de distancias con etiquetas en negrita si cumplen la condición
+            # Mostrar resultados de distancias con etiquetas en negrita se cumplen a condición
             for i, lonx_eslinga in enumerate([Lonx_eslinga_orelleta_1, Lonx_eslinga_orelleta_2, Lonx_eslinga_orelleta_3, Lonx_eslinga_orelleta_4], start=1):
                 if round(lonx_eslinga, 2) == round(Lonxitude_min_Eslinga, 2):
-                    st.success(f"**Lonx_eslinga_orelleta_{i}:** **{lonx_eslinga:.2f}**")
+                    #st.success(f"**Lonx_eslinga_orelleta_{i}:** **{lonx_eslinga:.2f}**")
+                    st.success(f"**Lonxitude eslinga orelleta {i}:** **{lonx_eslinga:.2f}**")
                 else:
-                    st.success(f"Lonx_eslinga_orelleta_{i}: {lonx_eslinga:.2f}")
+                    #st.success(f"Lonx_eslinga_orelleta_{i}: {lonx_eslinga:.2f}")
+                    st.success(f"Lonxitude eslinga orelleta {i}: {lonx_eslinga:.2f}")
 
-            st.success(f"Coordenadas de coord_o: X={coord_o_X:.2f}, Y={coord_o_Y:.2f}, Z={coord_o_Z:.2f}")
+            #st.success(f"Coordenadas de coord_o: X={coord_o_X:.2f}, Y={coord_o_Y:.2f}, Z={coord_o_Z:.2f}")
+            st.success(f"Coordenadas gancho (unión eslingas): X={coord_o_X:.2f}, Y={coord_o_Y:.2f}, Z={coord_o_Z:.2f}")
 
         except Exception as e:
-            st.error(f"Ocurrió un error: {e}")
+            st.error(f"Ocorreu un erro: {e}")
 
 if __name__ == "__main__":
     main()
